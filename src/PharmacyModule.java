d468254458a7fab1e2b75ed4ee07e838d241b6c1
Rachit <rachitrushilpaul@gmail.com>
2026-03-30 23:00:02 +0530

    Added Pharmacy module

---
+++ src/PharmacyModule.java
@@ -0,0 +1,7 @@
+public class PharmacyModule {
+
+    public static String findMedicine(String medicine) {
+
+        return "\nMedicine: " + medicine + " is available at nearby pharmacy.";
+    }
+}
